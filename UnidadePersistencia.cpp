5fd349328dbb81a630f6b3d56f493b1363f29696
carlosgabriel11 <cgaa1103@gmail.com>
2018-06-17 09:57:55 -0300

    Added the files of the use of sql

---
+++ UnidadePersistencia.cpp
@@ -0,0 +1,54 @@
+#include "UnidadePersistencia.hpp"
+
+list<ElementoResultado> ComandoSQL::listaResultado;
+
+EErroPersistencia::EErroPersistencia(string mensagem){
+        this->mensagem = mensagem;
+}
+
+string EErroPersistencia::what() {
+        return mensagem;
+}
+
+void ElementoResultado::setNomeColuna(const string& nomeColuna) {
+        this->nomeColuna = nomeColuna;
+}
+
+void ElementoResultado::setValorColuna(const string& valorColuna){
+        this->valorColuna = valorColuna;
+}
+
+void ComandoSQL::conectar() throw (EErroPersistencia) {
+      rc = sqlite3_open(nomeBancoDados, &bd);
+      if( rc != SQLITE_OK )
+        throw EErroPersistencia("Erro na conexao ao banco de dados");
+}
+
+void ComandoSQL::desconectar() throw (EErroPersistencia) {
+      rc =  sqlite3_close(bd);
+      if( rc != SQLITE_OK )
+        throw EErroPersistencia("Erro na desconexao ao banco de dados");
+}
+
+void ComandoSQL::executar() throw (EErroPersistencia) {
+        conectar();
+        rc = sqlite3_exec(bd, comandoSQL.c_str(), callback, 0, &mensagem);
+        if(rc != SQLITE_OK){
+                if (mensagem)
+                        free(mensagem);
+                throw EErroPersistencia("Erro na execucao do comando SQL");
+        }
+        desconectar();
+}
+
+int ComandoSQL::callback(void *NotUsed, int argc, char **valorColuna, char **nomeColuna){
+      NotUsed=0;
+      ElementoResultado elemento;
+      int i;
+      for(i=0; i<argc; i++){
+        elemento.setNomeColuna(nomeColuna[i]);
+        elemento.setValorColuna(valorColuna[i] ? valorColuna[i]: "NULL");
+        listaResultado.push_front(elemento);
+      }
+      return 0;
+}
---
+++ UnidadePersistencia.hpp
@@ -0,0 +1,59 @@
+#ifndef UnidadePersistenciaH
+#define UnidadePersistenciaH
+
+#include <list>
+
+#include <iostream>
+#include <conio.h>
+#include <string>
+#include <stdio.h>
+
+#include "sqlite3.h"
+
+using namespace std;
+
+class EErroPersistencia {
+private:
+        string mensagem;
+public:
+        EErroPersistencia(string);
+        string what();
+};
+
+
+class ElementoResultado {
+private:
+        string nomeColuna;
+        string valorColuna;
+public:
+        void setNomeColuna(const string&);
+        string getNomeColuna() const{
+            return nomeColuna;
+        }
+        void setValorColuna(const string&);
+        string getValorColuna() const{
+            return valorColuna;
+        }
+};
+
+class ComandoSQL {
+private:
+        const char *nomeBancoDados;
+        sqlite3 *bd;
+        char *mensagem;
+        int rc;
+        void conectar() throw (EErroPersistencia);
+        void desconectar() throw (EErroPersistencia);
+        static int callback(void *, int, char **, char **);
+
+public:
+        static list<ElementoResultado> listaResultado;
+        string comandoSQL;
+        ComandoSQL() {
+             //Informa o nome do banco de dados.
+             nomeBancoDados = "teste.db";
+        }
+        void executar() throw (EErroPersistencia);
+};
+
+#endif
