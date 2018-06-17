6ff73220ff23452c3f7311451c6a3860e2f76312
carlosgabriel11 <cgaa1103@gmail.com>
2018-06-17 19:19:53 -0300

    login 100%

--- MSAut.cpp
+++ MSAut.cpp
@@ -4,6 +4,64 @@
 ResultadoAutenticacao ServicoAutenticar::autenticar(const Email &email, const Senha &senha) throw(runtime_error) {
     //the object to be the result of the authentication
     ResultadoAutenticacao resultado;
+    //the object to access the database
+    ComandoSQL comando;
+    //the string that will carry the sql command
+    string query;
+    //the password of the user
+    string senhaDatabase;
 
-    return resultado;
+    //get password of the email
+    query = "SELECT Senha FROM Leitor \
+    WHERE Email = '" + email.getEmail() + "' \
+    UNION \
+    SELECT Senha from Desenvolvedor \
+    WHERE Email = '" + email.getEmail() + "' \
+    UNION \
+    SELECT Senha from Administrador \
+    WHERE Email = '" + email.getEmail() + "';";
+
+    //put the query into the object
+    comando.setComandoSQL(query);
+
+    //execute the command to get the password
+    try {
+        comando.executar();
+    }
+    catch (EErroPersistencia& e){
+        cerr << e.what();
+        system("pause");
+        resultado.setValor(Resultado::FALHA);
+        return resultado;
+    }
+
+    //get the password from the database
+    senhaDatabase = comando.listaResultado.back().getValorColuna();
+
+    //cleaning the list
+    comando.listaResultado.pop_back();
+
+    cout << senhaDatabase << endl;
+
+    //check if the email is on the database
+    if(senhaDatabase == ""){
+        cerr << "E-mail nao cadastrado" << endl;
+        system("pause");
+        resultado.setValor(Resultado::FALHA);
+        return resultado;
+    }
+
+    //check if the email is the same as typed
+    else if(senhaDatabase != senha.getSenha()){
+        cerr << "Senha incorreta" << endl;
+        system("pause");
+        resultado.setValor(Resultado::FALHA);
+        return resultado;
+    }
+
+    //authenticating
+    else{
+        resultado.setValor(Resultado::SUCESSO);
+        return resultado;
+    }
 }
